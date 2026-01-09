eaba9b6a0cf38e648d0932832d7230f3113f37a6
Rama krishna <batem6050@gmail.com>
2026-01-09 19:55:22 +0530

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -4,3 +4,18 @@ We built a router.
 
 Our chat model will decide to make a tool call or not based upon the user input
 We use a conditional edge to route to a node that will call our tool or simply end
+
+
+
+import os, getpass
+
+def _set_env(var: str):`
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"{var}: ")
+
+_set_env("OPENAI_API_KEY")
+
+_set_env("LANGSMITH_API_KEY")
+os.environ["LANGSMITH_TRACING"] = "true"
+os.environ["LANGSMITH_PROJECT"] = "langchain-academy"
+
