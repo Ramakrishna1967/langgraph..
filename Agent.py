9b7631734918e53f2a5684b646fa8480356228a4
Rama krishna <batem6050@gmail.com>
2026-01-09 19:54:42 +0530

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -1,1 +1,6 @@
+Agent
+Review
+We built a router.
 
+Our chat model will decide to make a tool call or not based upon the user input
+We use a conditional edge to route to a node that will call our tool or simply end
