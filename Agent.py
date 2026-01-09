c8ced552ee2734c8268043428c22c37623882cd2
Rama krishna <batem6050@gmail.com>
2026-01-09 19:56:57 +0530

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -56,7 +56,31 @@ llm_with_tools = llm.bind_tools(tools, parallel_tool_calls=False)
 from langgraph.graph import MessagesState
 from langchain_core.messages import HumanMessage, SystemMessage
 
-# System message
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import tools_condition
+from langgraph.prebuilt import ToolNode
+from IPython.display import Image, display
+
+# Graph
+builder = StateGraph(MessagesState)
+
+# Define nodes: these do the work
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+
+# Define edges: these determine how the control flow moves
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+react_graph = builder.compile()
+
+# Show
+display(Image(react_graph.get_graph(xray=True).draw_mermaid_png()))
 sys_msg = SystemMessage(content="You are a helpful assistant tasked with performing arithmetic on a set of inputs.")
 
 # Node
